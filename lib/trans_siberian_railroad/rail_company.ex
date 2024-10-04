b5b2d25d39f4ad524e0cafd04ee06e607b90bab3
chukinas <chukinas@gmail.com>
2024-10-04 10:28:27 -0400

    TransSiberianRailRoad.{Income,Location,Locations,RailCompany,RailLink,RailLinks,StockValue}

---
+++ lib/trans_siberian_railroad/rail_company.ex
@@ -0,0 +1,84 @@
+defmodule TransSiberianRailroad.RailCompany do
+  @moduledoc """
+  A rail company sells shares to players;
+  this is its main source of income.
+  A player with a controlling interest in a public company
+  can have that company build track, which is paid for by the company.
+  Building track increases a company's income,
+  which increases the dividends payout to stockholders.
+  """
+
+  use TypedStruct
+
+  @phase_1_ids ~w(red blue green yellow)a
+  @phase_2_ids ~w(black white)a
+  @all_ids @phase_1_ids ++ @phase_2_ids
+  @type id() :: :red | :blue | :green | :yellow | :black | :white
+
+  @type state() :: :waiting | :up_for_auction
+
+  typedstruct enforce: true do
+    field :id, id()
+    field :state, state()
+    field :money, non_neg_integer()
+    field :share_count, non_neg_integer()
+  end
+
+  #########################################################
+  # CONVERTERS (games states)
+  #########################################################
+
+  def private?(%__MODULE__{} = company) do
+    shares_sold(company) == 1 and not nationalized?(company)
+  end
+
+  def public?(%__MODULE__{} = company) do
+    shares_sold(company) >= 2 and not nationalized?(company)
+  end
+
+  def nationalized?(%__MODULE__{}) do
+    # TODO
+    false
+  end
+
+  defp shares_sold(%__MODULE__{id: id, share_count: share_count}) do
+    initial_share_count(id) - share_count
+  end
+
+  #########################################################
+  # HELPERS
+  #########################################################
+
+  defp initial_share_count(id) when id in @phase_1_ids, do: 5
+  defp initial_share_count(id) when id in @phase_2_ids, do: 3
+
+  # defp name(:red), do: "Красный"
+  # defp name(:blue), do: "Синий"
+  # defp name(:green), do: "Зелёный"
+  # defp name(:yellow), do: "Жёлтый"
+  # defp name(:black), do: "Чёрный"
+  # defp name(:white), do: "Белый"
+
+  #########################################################
+  # ALL
+  #########################################################
+
+  @type all() :: [t()]
+
+  @spec new_all() :: all()
+  def new_all() do
+    for id <- @all_ids do
+      %__MODULE__{
+        id: id,
+        state:
+          if id in @phase_1_ids do
+            :up_for_auction
+          else
+            :waiting
+          end,
+        money: 0,
+        share_count: initial_share_count(id)
+      }
+    end
+  end
+end
---
+++ test/trans_siberian_railroad/income_test.exs
@@ -0,0 +1,16 @@
+defmodule TransSiberianRailroad.IncomeTest do
+  use ExUnit.Case
+  alias TransSiberianRailroad.Income
+
+  test "There are 11 stock-value-increase spaces" do
+    assert 11 == Income.count_stock_value_increases(2, 70)
+  end
+
+  test "Increasing exactly to a stock-value-increase space increases stock value" do
+    assert 1 == Income.count_stock_value_increases(9, 10)
+  end
+
+  test "Increasing from 10 to 11 does not increase stock value" do
+    assert 0 == Income.count_stock_value_increases(10, 11)
+  end
+end
