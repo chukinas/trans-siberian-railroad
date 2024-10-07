4fa13c35f9f105ce30b87321983bf58c482a0eed
chukinas <chukinas@gmail.com>
2024-10-07 19:57:17 -0400

    TransSiberianRailroad.Aggregator.Auction
    - add Auction.get_current_bidder/1
    - rm Auction.current_bidder!/1
    - Arrange/Act/Assert

--- test/trans_siberian_railroad/aggregator/auction_test.exs
+++ test/trans_siberian_railroad/aggregator/auction_test.exs
@@ -12,21 +12,22 @@ defmodule TransSiberianRailroad.Aggregator.AuctionTest do
 
     case Enum.filter(game.events, &(&1.name == "auction_started")) do
       [%Event{payload: payload}] ->
-        assert Auction.current_bidder!(game.auction) == payload.current_bidder
+        current_bidder = Auction.get_current_bidder(game.auction)
+        assert current_bidder
+        assert current_bidder == payload.current_bidder
     end
 
     assert Auction.in_progress?(game.auction)
   end
 
-  # TODO what are the actual names of the three test sections?
   test "If all players pass on a railroad, a 'company_removed' event is generated" do
-    # Setup
+    # ARRANGE
     game = start_game_commands() |> game_from_commands()
     player_count = player_count(game.events)
 
-    # Do
+    # ACT
     {auction, _new_events} = Auction.state(game.events)
-    current_player = Auction.current_bidder!(auction)
+    assert current_player = Auction.get_current_bidder(auction)
 
     pass_commands =
       player_order(game.events)
@@ -35,8 +36,7 @@ defmodule TransSiberianRailroad.Aggregator.AuctionTest do
 
     game = handle_commands(game, pass_commands)
 
-    # Assert
-    IO.inspect(game)
+    # ASSERT
     assert length(filter_events_by_name(game.events, "company_passed")) == player_count
 
     assert %{company_id: :red} =
