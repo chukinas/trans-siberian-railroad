2c745c4beb68875ff91d546bee25db0b040ec5a7
chukinas <chukinas@gmail.com>
2024-10-17 10:50:22 -0400

    test phase 1 and 2 companies

--- lib/trans_siberian_railroad/metadata.ex
+++ lib/trans_siberian_railroad/metadata.ex
@@ -1,8 +1,22 @@
 defmodule TransSiberianRailroad.Metadata do
-  # TODO make a not somewhere of whether version is 0-based or 1-based
+  # TODO moduledoc
+  alias TransSiberianRailroad.Event
+
+  # TODO make a note somewhere of whether version is 0-based or 1-based
   # TODO this current only works for Auction. Need to generalize.
   def from_aggregator(%{last_version: version}, offset \\ 0) do
     [sequence_number: version + 1 + offset]
+  end
+
+  def from_events(events) do
+    current_version =
+      case events do
+        [%Event{sequence_number: version} | _] -> version
+        _ -> 0
+      end
+
+    # TODO unify the language
+    [sequence_number: current_version + 1]
   end
 
   defguard is(metadata) when is_list(metadata) and length(metadata) == 1
